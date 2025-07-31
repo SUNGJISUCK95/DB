330bdb57bda80bba5c5d29fa9463635ae33bc8d3
SUNGJISUCK95 <ebah1111@gmail.com>
2025-07-31 17:01:13 +0900

    java & mysql study 7/31 commit

--- mysql & java 연동.sql
+++ mysql & java 연동.sql
@@ -21,3 +21,69 @@ SELECT
 	start_date
 From department;
 DESC department;
+
+--
+
+INSERT INTO member(name, email)
+			values("김유신", "kim@test.com");
+DESC member;
+SELECT * FROM member;
+SELECT member_id,
+	name,
+    email,
+    created_at
+FROM member
+WHERE name = "홍길동";
+
+SELECT * FROM member;
+
+--  private int rno;
+-- 	private String mid;
+-- 	private String name;
+-- 	private String department;
+-- 	private int kor;
+-- 	private int eng;
+-- 	private int math;
+-- 	private String mdate;
+
+CREATE TABLE score_member(
+    mid        char(5)      PRIMARY KEY, -- "M0001" 형식의 트리거 적용
+    name       varchar(10)  NOT NULL,
+    department varchar(20),
+    kor        decimal(6,2) DEFAULT 0.0,
+    eng        decimal(6,2) DEFAULT 0.0,
+    math 	   decimal(6,2) DEFAULT 0.0,
+    mdate      datetime
+);
+
+SHOW TABLES;
+
+-- 트리거 생성
+
+/**************** TRIGGRER 시작 ******************/
+DELIMITER $$
+
+CREATE TRIGGER trh_score_member_mid
+BEFORE INSERT ON score_member
+FOR EACH ROW 
+BEGIN
+    DECLARE max_code INT;
+
+    SELECT IFNULL(MAX(CAST(RIGHT(mid, 4) AS UNSIGNED)), 0)
+    INTO max_code
+    FROM score_member;
+
+    SET NEW.mid = CONCAT('M', LPAD((max_code + 1), 4, '0'));
+END$$
+
+DELIMITER ;
+/**************** TRIGGRER 끝 ******************/
+
+SELECT count(*) FROM score_member; -- 행개수 size()에 값 전달시 사용 sql
+SELECT mid, name, department, kor, eng, math, mdate
+FROM score_member;
+
+DESC score_member;
+SELECT * FROM score_member;
+SELECT * FROM information_schema.triggers;
+
