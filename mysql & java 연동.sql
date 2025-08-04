511d2d1120d7d962801398897fdf42d3ae3ffcb1
SUNGJISUCK95 <ebah1111@gmail.com>
2025-08-04 18:16:48 +0900

    java & mysql study 8/04 commit

--- mysql & java 연동.sql
+++ mysql & java 연동.sql
@@ -221,7 +221,107 @@ SELECT	bid, btitle, author, price, isbn, bdate
 DESC book_tj;
 SELECT count(*) AS count FROM book_yes24;
 
+
 -- Connection 확인
 SHOW STATUS LIKE "Threads_connected";  -- 접속 커넥션 수
 SHOW PROCESSLIST;					   -- 활성중인 커넥션 (workbench도 하나의 커넥션이다.) (이클립스에서 main메소드 하나 실행할 때마다 커넥션 증가)
 SHOW VARIABLES LIKE "max_connections"; -- 최대 접속 가능 커넥션 수 (동시접속 가능한 수)
+
+
+--
+
+USE hrdb2019;
+SELECT DATABASE();
+
+/*****************************************************
+	book_market_books : 도서 테이블
+    book_market_member : 회원 테이블
+    book_market_cart : 장바구니 테이블    
+******************************************************/
+
+CREATE TABLE book_market_books (
+	bid    char(4)     PRIMARY KEY,
+    btitle varchar(50) NOT NULL,
+    author varchar(10),
+    price  int,
+    isbn   int,
+    bdate  datetime
+);
+
+CREATE TABLE book_market_member (
+	uid          varchar(20)     PRIMARY KEY,
+    password     varchar(20) NOT NULL,
+    create_date  datetime
+);
+
+CREATE TABLE book_market_cart (
+	order_id    int           AUTO_INCREMENT PRIMARY KEY,
+    uid         varchar(20),
+	bid         char(4),
+    btitle      varchar(50)   NOT NULL,
+    author      varchar(10),
+    price       int,
+    isbn        int,
+    order_date  datetime,
+    CONSTRAINT fk_uid FOREIGN KEY(uid)
+							REFERENCES book_market_member(uid),
+	CONSTRAINT fk_bid FOREIGN KEY(bid)
+							REFERENCES book_market_books(bid)
+);
+
+/**************** TRIGGRER 시작 ******************/
+DELIMITER $$
+
+CREATE TRIGGER trh_book_market_books_bid
+BEFORE INSERT ON book_market_books
+FOR EACH ROW 
+BEGIN
+    DECLARE max_code INT;
+
+    SELECT IFNULL(MAX(CAST(right(bid, 3) AS UNSIGNED)), 0)
+    INTO max_code
+    FROM book_market_books;
+
+    SET NEW.bid = CONCAT('B', LPAD((max_code + 1), 3, '0'));
+END$$
+
+DELIMITER ;
+/**************** TRIGGRER 끝 ******************/
+
+-- 	   bid    char(4)     PRIMARY KEY,
+--     btitle varchar(50) NOT NULL,
+--     author varchar(10),
+--     price  int,
+--     isbn   int,
+--     bdate  datetime
+INSERT INTO book_market_books(btitle, author, price, isbn, bdate) 
+				             values ('연금술사', '파울로 코엘료', '1000', FLOOR(RAND() * 9000) + 1000, now());
+INSERT INTO book_market_books(btitle, author, price, isbn, bdate) 
+				             values ('빅피처', '더글라스 케네디', '2000', FLOOR(RAND() * 9000) + 1000, now());
+INSERT INTO book_market_books(btitle, author, price, isbn, bdate) 
+				             values ('나미야 잡화점', '히가시노 게이고', '3000', FLOOR(RAND() * 9000) + 1000, now());
+
+INSERT INTO book_market_cart(bid, btitle, author, price, isbn, order_date)
+							SELECT bid, btitle, btitle, price, isbn, now()
+                            FROM book_market_books
+                            WHERE bid = 'B001';
+
+INSERT INTO book_market_member(uid, password, create_date) 
+				             values ('hong', '1234', now());
+
+SELECT * FROM book_market_member WHERE uid = 'hong' AND password = '1234';
+
+DELETE FROM book_market_cart WHERE uid = 'hong';
+
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE book_market_cart;
+SET FOREIGN_KEY_CHECKS = 1;
+
+DESC book_market_books;
+SELECT * FROM book_market_books;
+DESC book_market_member;
+SELECT * FROM book_market_member;
+DESC book_market_cart;
+SELECT * FROM book_market_cart;
+
+SELECT * FROM information_schema.triggers;
